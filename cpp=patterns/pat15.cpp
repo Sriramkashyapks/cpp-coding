bece77c6b902d074d3486238b9b27ad875371fc0
Sriram Kashyap <88237742+Sriramkashyapks@users.noreply.github.com>
2023-05-23 21:34:40 +0530

    Create pat15.cpp
    enter n
    4
    1 0 -1 -2 
    1 0 -1 
    1 0 
    1

---
+++ cpp=patterns/pat15.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,j,n;
+    cout<<"enter n"<<endl;
+    cin>>n;
+    for(i=1;i<=n;i++){
+     for(j=i;j<=n;j++){
+            cout<<i-j+1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
